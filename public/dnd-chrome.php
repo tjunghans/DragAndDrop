2e4cdeaa56cfe5c0a4a3f1c8ad4926688ebfcd10
Thomas Junghans <thomas.junghans@gmail.com>
2011-07-17 19:41:16 +0200

    -working example for ff, chrome and safari

--- public/dnd-chrome.php
+++ public/dnd-chrome.php
@@ -29,7 +29,7 @@
     </head>
     <body>
         <div draggable="false" id="source1">
-            <div draggable="true" data-value="draggable-item-1" id="dragme1" class="item"></div>
+            <div draggable="true" data-value="bar" id="dragme1" class="item"></div>
         </div>
         <hr/>
         <div dropzone="move s:text/html" id="target1">foo</div>
@@ -46,33 +46,20 @@
             var effect = 'copy';
             var format = 'text/html';
 
-/*
-            EventUtil.addHandler(target1, 'dragenter', function () {
-                target1.style.backgroundColor = 'blue';
-                target1.style.borderStyle = 'dashed';
-                target1.style.borderWidth = '2';
-                target1.style.borderColor = 'orange';
-            });
-
-            EventUtil.addHandler(target1, 'dragover', function (e) {
-                 e.preventDefault();
-            });
-*/
             EventUtil.addHandler(target1, 'drop', function (e) {
-                console.log('drop');
 
                 if (e.stopPropagation) {
                     e.stopPropagation();
                 }
 
                 var data = e.dataTransfer.getData(format);
-                console.log(data);
+
+                this.innerHTML = data;
                 
                 return false;
             });
 
             EventUtil.addHandler(target1, 'dragover', function (e) {
-                console.log('dragover');
 
                 if (e.preventDefault) {
                     e.preventDefault();
@@ -85,28 +72,12 @@
             });
 
             EventUtil.addHandler(target1, 'dragleave', function (e) {
-                console.log('dragleave');
                 this.className = '';
             });
 
             EventUtil.addHandler(target1, 'dragenter', function (e) {
-                console.log('dragenter');
-
                 e.dataTransfer.dropEffect = effect; // set as described on http://help.dottoro.com/ljffjemc.php
-                console.log('dropEffect:', e.dataTransfer.dropEffect); // In Firefox 5 this is set automatically
-                console.log(e.dataTransfer.effectAllowed);
                 var data = e.dataTransfer.getData(format);
-    
-
-
-
-                console.log('properties begin');
-                for (var x in e.dataTransfer) {
-
-                    console.log(e.dataTransfer[x]);
-
-                }
-                console.log('properties end');
 
                 return false;
 
@@ -116,16 +87,10 @@
             var d1 = document.getElementById('dragme1');
 
             EventUtil.addHandler(d1, 'dragstart', function (e) {
-                console.log('dragstart'); // should print dragstart
-                console.log(d1.draggable); // should print true
-
-                console.log(e.target);
-
                 var data = e.target.getAttribute('data-value');
 
                 e.dataTransfer.setData(format, data);
                 e.dataTransfer.effectAllowed = effect;
-                e.dataTransfer.setData('foo', 'bar');
 
                 return true;
             });
