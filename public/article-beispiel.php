4c84bc8e4d729c84e837b4f2350fb3b5cba8b965
Thomas Junghans <thomas.junghans@gmail.com>
2011-07-28 20:33:17 +0200

    -updated example

--- public/article-beispiel.php
+++ public/article-beispiel.php
@@ -5,8 +5,6 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>Artikelbeispiel</title>
 
-        <script type="text/javascript" src="javascripts/EventUtil.js"></script>
-
         <style type="text/css">
             *[draggable=true] {
                 -moz-user-select:none;
@@ -71,12 +69,80 @@
         <div id="Dropzone">Dropzone</div>
         <a draggable="true" href="#" id="Dragme">Drag Me</a>
 
+        <!-- Javascript -->
+        <script type="text/javascript">
+            var EventUtil = {
+                addHandler : function(element, type, handler){
+                    if (element.addEventListener){
+                          element.addEventListener(type, handler, false);
+                    } else if (element.attachEvent){
+                          element.attachEvent("on" + type, handler);
+                    } else {
+                          element["on" + type] = handler;
+                    }
+                },
+                removeHandler : function(element, type, handler){
+                    if (element.removeEventListener){
+                          element.removeEventListener(type, handler, false);
+                    } else if (element.detachEvent){
+                          element.detachEvent("on" + type, handler);
+                    } else {
+                          element["on" + type] = null;
+                    }
+                },
+                getCurrentTarget : function (e) {
+                    if (e.toElement) {
+                        return e.toElement;
+                    } else if (e.currentTarget) {
+                        return e.currentTarget;
+                    } else if (e.srcElement) {
+                        return e.srcElement;
+                    } else {
+                        return null;
+                    }
+                },
+                preventDefault : function (e) {
+                    e.preventDefault ? e.preventDefault() : e.returnValue = false;
+                },
+
+                /**
+                 * @author http://www.quirksmode.org/js/events_properties.html
+                 * @method getMousePosition
+                 * @param e
+                 */
+                getMousePosition : function (e) {
+                    var posx = 0,
+                        posy = 0;
+                    if (!e) {
+                        e = window.event;
+                    }
+
+                    if (e.pageX || e.pageY) 	{
+                        posx = e.pageX;
+                        posy = e.pageY;
+                    }
+                    else if (e.clientX || e.clientY) 	{
+                        posx = e.clientX + document.body.scrollLeft
+                            + document.documentElement.scrollLeft;
+                        posy = e.clientY + document.body.scrollTop
+                            + document.documentElement.scrollTop;
+                    }
+
+                    return {
+                        x : posx,
+                        y : posy
+                    };
+                }
+
+            };
+        </script>
+
         <script type="text/javascript">
 
             // 1. Ein paar Variablen definieren
             var dm = document.getElementById('Dragme'),
                 effect = 'move',
-                format = 'Text';
+                format = 'Text'; // Muss 'Text' oder 'Url' heissen.
 
             // 2. Eventhandler DRAGSTART
             EventUtil.addHandler(dm, 'dragstart', function (e) {
